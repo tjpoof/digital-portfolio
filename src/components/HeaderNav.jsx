29e974632ae19fabed5779b6e54af09a92504b73
tjh827 <tj2002@gmail.com>
2025-07-17 15:42:59 -0500

    started on navbar

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -3,32 +3,36 @@ import { NavLink } from 'react-router-dom';
 
 export function HeaderNav() {
     return (
-        <header id="top-header" className="header-nav">
-            <nav className="navbar navbar-expand-sm navbar-dark bg-dark container-fluid">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/">
-                            <span className="nav-title">Home</span>
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/about">
-                            <span className="nav-title">About Me</span>
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/#projects">
-                            <span className="nav-title">Projects</span>
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to="/#contact">
-                            <span className="nav-title">Contact</span>
-                        </NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark container-fluid">
+            <div className="name-container">
+                <NavLink className="nav-link" to="/">
+                    TJ Harrison
+                </NavLink>
+            </div>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/">
+                        <span className="nav-title">Home</span>
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/about">
+                        <span className="nav-title">About Me</span>
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/#projects">
+                        <span className="nav-title">Projects</span>
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/#contact">
+                        <span className="nav-title">Contact</span>
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+
     )
 }
 
